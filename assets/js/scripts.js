e4d570d4c5fb4301a29c4ed391db4f53a01cd5ed
HemantDutta <hemantnorddutta2@gmail.com>
2022-12-10 00:15:03 +0530

    navbar animation

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,4 +2,8 @@ function onloadAnimations(){
     let footer = document.getElementById('footer');
     footer.style.transform = "translateY(0%)";
     footer.style.opacity = "100%";
+
+    let navbar = document.getElementById('navbar');
+    navbar.style.transform = "translateY(0%)"
+    navbar.style.opacity = "100%"
 }
