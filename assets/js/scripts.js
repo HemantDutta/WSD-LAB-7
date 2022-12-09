f9f7ff7fe5ad2c0537d38b5170ff16f053763ade
HemantDutta <hemantnorddutta2@gmail.com>
2022-12-10 01:28:47 +0530

    animations complete

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,5 +5,13 @@ function onloadAnimations(){
 
     let navbar = document.getElementById('navbar');
     navbar.style.transform = "translateY(0%)"
-    navbar.style.opacity = "100%"
+    navbar.style.opacity = "100%";
+
+    let jsonDiv = document.getElementById('jsonDiv');
+    jsonDiv.style.transform = "translateY(0%)"
+    jsonDiv.style.opacity = "100%";
+
+    let searchDiv = document.getElementById('searchDiv');
+    searchDiv.style.transform = "translateY(0%)"
+    searchDiv.style.opacity = "100%";
 }
