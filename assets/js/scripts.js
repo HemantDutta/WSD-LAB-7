f21d75982ab2b28b60c727e2914c11e347f9b66c
HemantDutta <hemantnorddutta2@gmail.com>
2022-12-10 10:58:36 +0530

    angular handling

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -17,3 +17,19 @@ function onloadAnimations(){
 }
 
 
+
+angular.module('myApp', []).controller('myCtrl', function ($scope){
+    $scope.data = [
+        {name: 'Hemant', category: 'Web Dev', Rating: 4, Gigs: 5, Reviews: 2, Price: 1500, Joined: '10-12-2022', Status: 'active'},
+        {name: 'Darpan', category: 'Manager', Rating: 2, Gigs: 1, Reviews: 0, Price: 500, Joined: '10-12-2022', Status: 'active'},
+        {name: 'Rohan', category: 'C Dev', Rating: 5, Gigs: 10, Reviews: 8, Price: 5000, Joined: '10-12-2022', Status: 'blocked'},
+        {name: 'Piyush', category: '', Rating: 2, Gigs: 1, Reviews: 0, Price: 500, Joined: '10-12-2022', Status: 'active'},
+        {name: 'Darpan', category: 'Manager', Rating: 2, Gigs: 1, Reviews: 0, Price: 500, Joined: '10-12-2022', Status: 'active'},
+        {name: 'Darpan', category: 'Python', Rating: 2, Gigs: 1, Reviews: 0, Price: 500, Joined: '10-12-2022', Status: 'active'},
+        {name: 'Darpan', category: 'Manager', Rating: 2, Gigs: 1, Reviews: 0, Price: 500, Joined: '10-12-2022', Status: 'active'},
+        {name: 'Darpan', category: 'Manager', Rating: 2, Gigs: 1, Reviews: 0, Price: 500, Joined: '10-12-2022', Status: 'active'},
+        {name: 'Darpan', category: 'Manager', Rating: 2, Gigs: 1, Reviews: 0, Price: 500, Joined: '10-12-2022', Status: 'active'},
+    ];
+    $scope.rows = 1;
+});
+
