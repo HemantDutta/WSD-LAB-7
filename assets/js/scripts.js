a6ff012b7dc262ae6a82922091e180dc7f550612
HemantDutta <hemantnorddutta2@gmail.com>
2022-12-11 16:55:17 +0530

    status custom filters

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,4 @@
-function onloadAnimations(){
+function onloadAnimations() {
     let footer = document.getElementById('footer');
     footer.style.transform = "translateY(0%)";
     footer.style.opacity = "100%";
@@ -17,9 +17,8 @@ function onloadAnimations(){
 }
 
 
-
 let app = angular.module('myApp', []);
-app.controller('myCtrl', function ($scope){
+app.controller('myCtrl', function ($scope) {
     $scope.data = [
         {name: 'Hemant', category: 'Web Dev', Rating: 4, Gigs: 5, Reviews: 2, Price: 1500, Joined: '10-12-2022', Status: 'active'},
         {name: 'Darpan', category: 'Manager', Rating: 2, Gigs: 1, Reviews: 0, Price: 500, Joined: '10-12-2022', Status: 'active'},
@@ -33,23 +32,43 @@ app.controller('myCtrl', function ($scope){
     ];
     $scope.rows = 1;
 });
-app.filter('reverse', function (){
-    return function (text){
+app.filter('reverse', function () {
+    return function (text) {
         text = text || ''; //Since angular has lazy loading, text will only be initialized after some value has been entered, so this statement checks if text has a value otherwise it initializes it with an empty string.
         let out = '';
-        for(let i = text.length-1; i>=0; i--){
+        for (let i = text.length - 1; i >= 0; i--) {
             out += text[i];
         }
         return out;
     }
 });
 
-app.filter('addRup', function (){
-    return function (text){
+app.filter('addRup', function () {
+    return function (text) {
         text = text || '';
         let out = '';
-        out += "₹"+text;
+        out += "₹" + text;
         return out;
     }
-})
+});
+
+app.filter('addStar', function () {
+    return function (text) {
+        text = text || '';
+        let out = '';
+        out += text + "⭐";
+        return out;
+    }
+});
+
+app.filter('checkStat', function () {
+    return function (text) {
+            if (text === 'active') {
+                text+=" ✔";
+            } else {
+                text+=" ❌";
+            }
+        return text;
+    }
+});
 
